453d82e12645fdb9bdf1e32c10a86424847c10de
Lars Levie <larslevie@gmail.com>
2019-06-28 09:18:52 -0500

    Test payload builder

--- tests/config.test.js
+++ tests/config.test.js
@@ -1,30 +1,23 @@
-const chai = require('chai');
+const { expect } = require('chai');
 const config = require('../src/config');
-
-const { expect } = chai;
 
 describe('config', () => {
   it('loads app from a JSON config file', () => {
-    expect(config).to.eql({
-      apps: {
-        mack: {
-          configRepo: 'realgeeks/geekstack',
-          configRepoPath: 'kube-config/infra/mack',
-          defaultBranch: 'master',
-          imageName: '558529356944.dkr.ecr.us-east-1.amazonaws.com/mack',
-          name: 'mack',
-          requiredStatuses: ['ci/circleci', 'docker/builder'],
-          strategy: 'kubernetes/kustomize',
-          targets: ['am1', 'am21'],
-        },
-      },
-      repos: {
-        'realgeeks/geekstack': {
-          branch: 'master',
-          name: 'realgeeks/geekstack',
-          url: 'git@github.com:RealGeeks/geekstack.git',
-        },
-      },
+    expect(config.apps.mack).to.eql({
+      configRepo: 'realgeeks/geekstack',
+      configRepoPath: 'kube-config/infra/mack',
+      defaultBranch: 'master',
+      imageName: '558529356944.dkr.ecr.us-east-1.amazonaws.com/mack',
+      name: 'mack',
+      requiredStatuses: ['ci/circleci', 'docker/builder'],
+      strategy: 'kubernetes/kustomize',
+      targets: ['am1', 'am21'],
+    });
+
+    expect(config.repos['realgeeks/geekstack']).to.eql({
+      branch: 'master',
+      name: 'realgeeks/geekstack',
+      url: 'git@github.com:RealGeeks/geekstack.git',
     });
   });
 });
---
+++ tests/functions/build-payload.test.js
@@ -0,0 +1,45 @@
+const { expect } = require('chai');
+const buildPayload = require('../../src/functions/build-payload');
+
+describe('build-payload', () => {
+  const validArgs = {
+    name: 'basic',
+    target: 'am1',
+    ref: 'production',
+    adapter: '',
+    room: '',
+    user: {},
+  };
+
+  it('Throws if app is missing', () => {
+    const args = Object.assign({}, validArgs, { name: 'missingApp' });
+
+    expect(() => buildPayload(args), args).to.throw('Config for missingApp: missing or malformed');
+  });
+
+  it('Assumes master if ref missing and defaultBranch unset', () => {
+    const args = Object.assign({}, validArgs);
+    delete args.ref;
+
+    const payload = buildPayload(args);
+
+    expect(payload.source.branch).to.eql('master');
+  });
+
+  it('Uses ref if provided', () => {
+    const args = Object.assign({}, validArgs);
+    const payload = buildPayload(args);
+
+    expect(payload.source.branch).to.eql('production');
+  });
+
+  it('Uses defaultBranch if set and ref missing', () => {
+    const args = Object.assign({}, validArgs, { name: 'withDefaults' });
+
+    delete args.ref;
+
+    const payload = buildPayload(args);
+
+    expect(payload.source.branch).to.eql('foobar');
+  });
+});
